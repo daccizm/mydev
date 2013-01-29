6608a013947c406b1eee035b7dde329c97cdc78b
fukuishere <fukuishere@gmail.com>
2013-01-29 20:57:54 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maoyu::Application.config.secret_token = '2b04c05ddf8210fab6b886fca8ae15e19219566ae6fbac3213fb0f9212ae62c335bc1dd0a9eee0e07dc175a4854c2d89fb277541285571e156e1e2c8ca3078f6'
